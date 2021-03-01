389b94e69397212d4a20dc3196a53bd783e5d2e6
Richard Torenvliet <richard@torenvliet.org>
2021-03-01 23:23:02 +0100

    Blog update

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Notes on setting up AWS S3 Pre-signed URLs",'url':"/aws/presigned-urls/s3/devops/2021/01/29/presigned-urls.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Notes on setting up AWS S3 Pre-signed URLs",'url':"/aws/presigned-urls/s3/devops/2021/01/29/presigned-urls.html"},{'title':"How to split up Docker files (For Faster Deploys)",'url':"/aws/docker/split/up/file/2021/03/01/splitting-up-docker-files.html"}]};
